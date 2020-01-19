3f5c91d0fe53e51de3295389b3a3a5233bd1ab8f
Jefferson <jeffsmmatos@gmail.com>
2020-01-19 10:02:55 -0300

    Alterando para bindValue no arquivo de implemtentação

--- public_html/ImplementandoBD.php
+++ public_html/ImplementandoBD.php
@@ -5,8 +5,8 @@ class ImplementandoBD extends DataBase
     public  function setcadastro($rg)
     {
         try {
-            $stmt = $this->conn->prepare("INSERT INTO teste(rg) VALUES ('$rg')");
-            $stmt->bindParam(':rg', $rg, PDO::PARAM_STR);
+            $stmt = $this->conn->prepare("INSERT INTO teste(rg) VALUES (:rg)");
+            $stmt->bindValue(':rg', $rg);
             $stmt->execute();
             return true;
         } catch (PDOException $ex) {
